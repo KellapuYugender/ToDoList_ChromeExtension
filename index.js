f255e4052c2e3d94bd8c7ce761f5f4470b9b03b7
KellapuYugender <kellapuyugender@gmail.com>
2026-05-17 15:26:33 +0530

    Rendering tasks

---
+++ index.js
@@ -0,0 +1,35 @@
+let myTasks = [];
+let idGiver = 1;
+const inputEl = document.getElementById("input-id");
+const addBtnEl = document.getElementById("add-btn-id");
+const delBtnEl = document.getElementsByClassName("delete-btn");
+const ulEl = document.getElementById("ul-id");
+
+addBtnEl.addEventListener("click", (e) => {
+  myTasks.push({
+    id: idGiver++,
+    task: inputEl.value,
+  });
+  inputEl.value = "";
+  console.log(myTasks);
+
+  render(myTasks);
+});
+
+delBtnEl.addEventListener("click", (e) => {
+  console.log(e.target);
+});
+
+function render(tasks) {
+  let listItems = "";
+
+  for (let i = 0; i < tasks.length; i++) {
+    listItems += `
+    <li> ${tasks[i].task}
+
+     <button class= "delete-btn" id="${i + 1}">X</button>
+
+    </li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
